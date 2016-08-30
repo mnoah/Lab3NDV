71567ce9fdc78f3d5cfaa1834e294d64d6e2ca65
mnoah <noahvrazo@gmail.com>
2016-08-30 09:45:58 -0400

    final

---
+++ Lab3NDV/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3NDV
+{
+    public class Class1
+    {
+        public int square( int InPutNum)
+        {
+          int squareNum = (InPutNum * InPutNum);
+
+            return squareNum;
+        }
+
+        public int cube(int InPutNum2)
+        {
+           int cubeNum = (InPutNum2 * InPutNum2 * InPutNum2);
+            return cubeNum;
+        }
+
+           
+    }
+}
--- Lab3NDV/Program.cs
+++ Lab3NDV/Program.cs
@@ -10,6 +10,40 @@ namespace Lab3NDV
     {
         static void Main(string[] args)
         {
+            string Awn;
+            do
+            {
+                Class1 Calc = new Class1();
+                Console.WriteLine("Learn your squres and cubes\n");
+
+
+                Console.WriteLine("Enter an Integer:");
+                string InPut = Console.ReadLine();
+                int InPutNum = int.Parse(InPut);
+
+
+                int InPutNum2 = 1;
+                Console.WriteLine("Number      Squered       Cubed");
+                Console.WriteLine("======      =======       =====");
+
+                for (int i = 0; i < InPutNum; i++)
+                {
+                    Console.WriteLine(InPutNum2 +
+                            "           "
+                            + Calc.square(InPutNum2)
+                            + "          "
+                            + Calc.cube(InPutNum2));
+                    InPutNum2++;
+                }
+                Console.WriteLine("Continue? (y/n)");
+                Awn = Console.ReadLine();
+            } while (Awn == "y");
+
+
+
+
+
+
         }
     }
 }
